083c2a55c30368a295dfe6d52ad78acfeb61163e
Julian Koripamo <jkoripamo@gmail.com>
2023-11-01 14:59:45 +0100

    intro screen

---
+++ lib/screens/intro_screen.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:sneaker_shop/screens/home_screen.dart';
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(25.0),
+              child: Image.asset(
+                'assets/images/adidas.png',
+                height: 190,
+              ),
+            ),
+            const SizedBox(height: 50.0),
+            const Text(
+              'Adidas is All In.',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 24),
+            const Text(
+              'Sneakers made with your comfort in mind so you can put all of your focus into your next session.',
+              style: TextStyle(fontSize: 15, color: Colors.grey),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 50),
+            GestureDetector(
+              onTap: () => Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const HomeScreen(),
+                ),
+              ),
+              child: Container(
+                margin: const EdgeInsets.all(40),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  color: Colors.grey[900],
+                ),
+                padding: const EdgeInsets.all(25),
+                child: const Center(
+                  child: Text(
+                    'Shop now',
+                    style: TextStyle(color: Colors.white, fontSize: 16.0),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
