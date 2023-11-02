1186e8ebce6d8dae19b7679dd474644d55e48b9d
Julian Koripamo <jkoripamo@gmail.com>
2023-11-02 11:39:29 +0100

    cart model

---
+++ lib/models/cart.dart
@@ -0,0 +1,60 @@
+import '../models/shoe.dart';
+
+class Cart {
+  List<Shoe> shoeList = [
+    // List of shoes for sale
+    Shoe(
+      name: 'Lite Racer Adapt 6.0',
+      price: '70',
+      description: '',
+      imagePath: 'assets/images/Lite_Racer_Adapt_6.0.png',
+    ),
+    Shoe(
+      name: 'X_PLRPHASE',
+      price: '110',
+      description: '',
+      imagePath: 'assets/images/X_PLRPHASE.png',
+    ),
+    Shoe(
+      name: 'OZWEEGO',
+      price: '120',
+      description: '',
+      imagePath: 'assets/images/OZWEEGO.png',
+    ),
+    Shoe(
+      name: 'Samba OG',
+      price: '100',
+      description: '',
+      imagePath: 'assets/images/samba_OG.png',
+    ),
+    Shoe(
+      name: 'Hoops 3.0 MID',
+      price: '80',
+      description: '',
+      imagePath: 'assets/images/hOOPS_3.0_Mid.png',
+    ),
+  ];
+
+  // List of items in user cart
+  List<Shoe> userCart = [];
+
+  // get list of shoes for sale
+  List<Shoe> getShoeList() {
+    return shoeList;
+  }
+
+  // get user cart
+  List<Shoe> getUserCart() {
+    return userCart;
+  }
+
+  // add item to cart
+  void addItemToCart(Shoe shoe) {
+    userCart.add(shoe);
+  }
+
+  // remove item from cart
+  void removeItemFromCart(Shoe shoe) {
+    userCart.remove(shoe);
+  }
+}
